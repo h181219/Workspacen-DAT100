f16c3f20005453e3a15987b146ca999d57780b9a
eivas <eivas@158.37.235.84>
2016-08-24 11:52:22 +0200

    .

---
+++ Velkommen/src/no/hib/dat100/Velkommen.java
@@ -0,0 +1,10 @@
+package no.hib.dat100;
+
+public class Velkommen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Nå virker det!");
+	}
+
+}
